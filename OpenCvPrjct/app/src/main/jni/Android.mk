d23f763fad09158218a08e71c4e3b5ab3af9822f
Ahmad <ahmadjsalem@gmail.com>
2015-06-15 14:28:04 -0400

    Not Scratch yet

--- OpenCvPrjct/app/src/main/jni/Android.mk
+++ OpenCvPrjct/app/src/main/jni/Android.mk
@@ -2,8 +2,13 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-include OpenCV.mk
-LOCAL_C_INCLUDE:= include/
+
+OPENCVROOT:= /Users/ahmadsalem/Documents/OpenCV-android-sdk
+OPENCV_CAMERA_MODULES:=on
+OPENCV_INSTALL_MODULES:=on
+OPENCV_LIB_TYPE:=SHARED
+
+include ${OPENCVROOT}/sdk/native/jni/OpenCV.mk
 
 LOCAL_MODULE    := mixed_sample
 LOCAL_SRC_FILES := jni_part.cpp
